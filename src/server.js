734f51cc884835720e85df785c3ccbe230391702
Luceta <57941164+Luceta@users.noreply.github.com>
2022-03-27 23:28:24 +0900

    add : answers

--- src/server.js
+++ src/server.js
@@ -16,9 +16,16 @@ httpServer.listen(3000, handleListen);
 const io = new Server(httpServer);
 
 io.on("connection", (socket) => {
-  socket.on("join_room", (roomName, done) => {
+  socket.on("join_room", (roomName) => {
     socket.join(roomName);
-    done();
     socket.to(roomName).emit("welcome");
+  });
+
+  socket.on("offer", (offer, roomName) => {
+    socket.to(roomName).emit("offer", offer);
+  });
+
+  socket.on("answer", (answer, roomName) => {
+    socket.to(roomName).emit("answer", answer);
   });
 });
